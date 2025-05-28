6ff36333746bfa9956e877b75a7ca93d3c6b1512
Jeff <2548278761@qq.com>
2025-05-28 08:41:48 +0800

    update

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "first post",
-        "excerpt":"Hello Minimal Mistakes! Hello Blog!  ","categories": ["another"],
-        "tags": ["GitHub","GitHub Pages"],
-        "url": "/another/2025/05/27/first-post.html",
-        "teaser": null
-      }]
