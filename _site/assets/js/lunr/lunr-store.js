733450046f73cc378049c433b649651a6d311d06
Jeff <2548278761@qq.com>
2025-06-28 21:50:15 +0800

    Docs(2025-06-23-Unity-Invisible-Trap.md): Add Class4, 5, 6; Add Quick List

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -300,8 +300,8 @@ var store = [{
         "teaser": null
       },{
         "title": "Unity Invisible Trap",
-        "excerpt":"Unity实际开发中，有很多“看起来正常、实则容易出错”的奇怪问题（或称为隐形陷阱） Class 1 交互相关 1.多个可交互物体，输入触发多个 场景：靠近多个箱子同时按下E，多个箱子一起打开 原因：所有物体监听E，没有加距离或唯一判定 解决方案：只响应最近/朝向前方的一个对象 2.Trigger触发多次Enter/Exit 场景：一个对象靠近某个Trigger区域，OnTriggerEnter被触发两次 原因： 可能身上挂有多个Collider 或包含子物体的Collider也触发了 解决方案：确认是否是重复触发，加入other.gameObject == expectedObject判断 3.Button UI点击两次才响应 原因： UI按钮背后有透明/未关闭的UI元素挡住射线 或者EventSystem被禁用、Canvas未设置正确Sorting Order 解决方案：调试Graphic Raycaster、Canvas排序、Raycast Target勾选项 Class 2 物理系统相关 1.Rigidbody设置position导致穿透 使用transform.position = ...设置刚体位置，会跳过物理引擎检测，导致穿墙 正确做法：使用Rigidbody.MovePosition() 2.Collider和Rigidbody的组合错误 常见错误组合 静态物体（如地面）用了非Kinematic的Rigidbody 移动物体没加Rigidbody，靠transform移动导致物理行为异常 Unity的推荐： 静态物体（地面）用Collider，无Rigidbody 动态物体加Rigidbody，控制用物理接口 Class 3 生命周期相关 1.协程中的WaitForSeconds在TimeScale为0时失效 StartCoroutine(Example()); IEnumerator Example() { yield return...","categories": ["Debug"],
-        "tags": ["Unity","Unity Trap","Debug"],
+        "excerpt":"Unity实际开发中，有很多“看起来正常、实则容易出错”的奇怪问题（或称为隐形陷阱） Quick List Class1 交互相关 现象 原因 解决方式 标签 按下交互键时多个箱子同时打开 范围内存在多个交互体，未判断距离 使用 OverlapSphere + 计算最近距离 输入、交互 鼠标点击物体无反应 被 UI 或透明物体挡住，Raycast 被拦截 检查 UI 的 Raycast Target 设置 输入、射线 射线打不中目标 射线层级错误或未设置 LayerMask 使用正确 Layer 和 LayerMask 输入、射线 玩家进入 Trigger 区域被触发两次 存在多个 Collider 重叠，重复触发 判断 other.gameObject 是否重复 触发器、碰撞 Class2 动画控制 现象 原因...","categories": ["Debug"],
+        "tags": ["Unity","Debug Log"],
         "url": "/debug/2025/06/01/Unity-Invisible-Trap.html",
         "teaser": null
       },{
